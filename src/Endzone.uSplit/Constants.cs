6a3084aa39d50a0411f5b0567beeff37f093fcc7
Ondrej Pialek <ondrej@pialek.eu>
2015-06-25 17:15:38 +0700

    Created the uSplit application (section) with a nice logo.

---
+++ src/Endzone.uSplit/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Endzone.uSplit
+{
+    internal static class Constants
+    {
+        public const string PluginName = "uSplit";
+        public const string ApplicationName = "uSplit";
+        public const string ApplicationAlias = "usplit";
+        public const string AreaName = "usplit";
+        public const string TreeAlias = "experiments";
+    }
+}
---
+++ src/Endzone.uSplit/uSplitApplication.cs
@@ -0,0 +1,10 @@
+﻿using umbraco.businesslogic;
+using umbraco.interfaces;
+
+namespace Endzone.uSplit
+{
+    [Application(Constants.ApplicationAlias, Constants.ApplicationName, "icon-split-alt", 11)]
+    public class uSplitApplication : IApplication
+    {
+    }
+}
