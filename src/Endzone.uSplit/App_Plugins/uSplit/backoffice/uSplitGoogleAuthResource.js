98c39f11ff4d70a9d6e1d704a309f02b0d60d06d
Ondřej Piálek <ondrej@pialek.eu>
2016-05-20 13:49:35 -0600

    Updated package metadata and package information

--- src/Endzone.uSplit/App_Plugins/uSplit/backoffice/uSplitGoogleAuthResource.js
+++ src/Endzone.uSplit/App_Plugins/uSplit/backoffice/uSplitGoogleAuthResource.js
@@ -3,15 +3,7 @@
         return {
             getStatus: function () {
                 return $http.get("backoffice/uSplit/GoogleApi/Status");
-            },
-
-            //reauthorize: function (documentTypeId) {
-            //    return $http.get("backoffice/uSplit/GoogleApi/DoAuth", {
-            //        params: {
-            //            contentTypeId: documentTypeId
-            //        }
-            //    });
-            //}
+            }
         }
     }
 );
