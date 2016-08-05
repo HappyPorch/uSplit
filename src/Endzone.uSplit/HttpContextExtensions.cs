aa6eb434983e63d976fe13d0b0307992df15f995
Ondřej Piálek <ondrej@pialek.eu>
2016-08-04 19:00:14 -0700

    Pasted code for an extension method added in 7.4. so that uSplit works on older v7 sites and does not require site upgrade.

---
+++ src/Endzone.uSplit/HttpContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using Umbraco.Web;
+
+namespace Endzone.uSplit
+{
+    public static class HttpContextExtensions
+    {
+        private const string HttpContextItemName = "Umbraco.Web.UmbracoContext";
+        
+        [Obsolete("This method was introduced in Umbraco 7.4. This method and class can safely be removed once support for earlier versions is no longer needed.")]
+        public static UmbracoContext GetUmbracoContext(this HttpContextBase http)
+        {
+            if (http.Items.Contains(HttpContextItemName))
+            {
+                var umbCtx = http.Items[HttpContextItemName] as UmbracoContext;
+                return umbCtx;
+            }
+            return null;
+        }
+    }
+}
