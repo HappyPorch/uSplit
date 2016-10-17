5c1bbaabb01b11bc499997ae0bb45266f6d8b502
Ondřej Piálek <ondrej@pialek.eu>
2016-10-16 18:42:40 -0700

    Added a null check for Umbraco Context, not all requests are coming via Umbraco!

--- src/Endzone.uSplit/Pipeline/VariationReportingActionFilterAttribute.cs
+++ src/Endzone.uSplit/Pipeline/VariationReportingActionFilterAttribute.cs
@@ -2,48 +2,61 @@ using System;
 using System.Web;
 using System.Web.Mvc;
 using Endzone.uSplit.Models;
+using Umbraco.Core.Logging;
 
 namespace Endzone.uSplit.Pipeline
 {
     public class VariationReportingActionFilterAttribute : ActionFilterAttribute
     {
+        private Logger logger;
+
         private const string FilteringDoesntWork =
             "uSplit cannot insert a required JS fragment (to report chosen A/B testing variation) to your page " +
             "automatically. Add a call to @Html.RenderAbTestingScriptTags() into your master layout " +
             "to manually include this JS snippet and suppress this error.";
 
+        public VariationReportingActionFilterAttribute()
+        {
+            logger = Logger.CreateWithDefaultLog4NetConfiguration();
+        }
+
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            //ignore child actions, cannot use filter with them
-            if (filterContext.IsChildAction)
-                return;
-
-            var httpContext = filterContext.HttpContext;
-            var umbracoContext = httpContext.GetUmbracoContext();
-            var request = umbracoContext.PublishedContentRequest;
-            var response = httpContext.Response;
-
-            if (response.ContentType != "text/html")
-                return; //we only know how to report from JavaScript, so we need to be serving an HTML page
-
-            var variedContent = request?.PublishedContent as VariedContent;
-            if (variedContent == null)
-                return; //this is not a variation, not part of an experiment
-
-            Func<bool> scriptsNeeded =
-                () => Equals(filterContext.HttpContext.Items[Constants.VariationReportedHttpContextItemsKey], true);
-
             try
             {
+                //ignore child actions, cannot use filter with them
+                if (filterContext.IsChildAction)
+                    return;
+
+                var httpContext = filterContext.HttpContext;
+                var umbracoContext = httpContext.GetUmbracoContext();
+                var request = umbracoContext?.PublishedContentRequest;
+                if (request == null)
+                    return; //not a request for an umbraco page
+
+                var response = httpContext.Response;
+                if (response.ContentType != "text/html")
+                    return; //we only know how to report from JavaScript, so we need to be serving an HTML page
+
+                var variedContent = request?.PublishedContent as VariedContent;
+                if (variedContent == null)
+                    return; //this is not a variation, not part of an experiment
+
+                Func<bool> scriptsNeeded = () => Equals(filterContext.HttpContext.Items[Constants.VariationReportedHttpContextItemsKey], true);
+
                 response.Filter = new VariationReportingHttpResponseFilter(response.Filter, variedContent, scriptsNeeded);
             }
-            catch (HttpException)
+            catch (Exception e)
             {
                 //There are instances where filtering simply doesn't work
                 //https://github.com/EndzoneSoftware/uSplit/issues/4
 
+                logger.Error(GetType(), "An exception has been throw when uSplit tried to report a variation to Google Analytics." +
+                                        "This may happen due to incompatibility with thrid party libraries or custom code.", e);
+
                 if (!filterContext.HttpContext.Items.Contains(Constants.VariationReportedHttpContextItemsKey))
                     filterContext.HttpContext.Items[Constants.VariationReportedHttpContextItemsKey] = false;
+
             }
         }
 
